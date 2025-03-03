10ef52ccd0c1ba68c750db20c769fa946d8c9d36
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-03 06:35:19 +0000

    save to draft

---
+++ rgfhj/macros/fghm.sql
@@ -0,0 +1,4 @@
+
+{%- macro fghm(parameter1) -%}
+    select * from {{ parameter1 }}
+{%- endmacro -%}
---
+++ rgfhj/models/ghjghj/model_ghjghj_Aggregate_1.sql
@@ -0,0 +1,20 @@
+{{
+  config({    
+    "materialized": "table",
+    "alias": "prophecy__temp_ghjghj_post_Aggregate_1_0",
+    "database": "database",
+    "schema": "default"
+  })
+}}
+
+WITH Aggregate_1 AS (
+
+  SELECT * 
+  
+  FROM `` AS in0
+
+)
+
+SELECT *
+
+FROM Aggregate_1
