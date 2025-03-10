21bfd0bdfead5272827d572a4a20d7b97ce83fb7
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-10 20:23:58 +0000

    Cloning prophecy project main branch

---
+++ mklmklmkl/gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'external_sql_simple_project',
+    version = '0.0.1',
+    packages = ['external_sql_simple_project'],
+    package_dir = {'external_sql_simple_project': '.'},
+    description = '',
+    install_requires = [],
+)
+
